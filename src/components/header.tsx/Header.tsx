fd37f9797001fda108ef55ced660911eb68ba0f4
Joshua Lucas <joshuamlucas91@gmail.com>
2021-05-21 22:46:18 -0400

    refactor(header): restyled the header component.

--- src/components/header.tsx/Header.tsx
+++ src/components/header.tsx/Header.tsx
@@ -2,18 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import styles from '../../styles/components/header.module.scss'
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function Header() {
-    return(
-        <>
-            <h1 className={styles.title}>This is my portfolio </h1>
-            <Image
-                src='/images/LUCAS-SPRING-4_websize.jpg'
-                alt="Image of me and my family"
-                layout="fill"
-            />
-            {/* Put transparent box with title */}
-        </>
-    )
+  return (
+    <div className={styles.component}>
+      <div className={styles.heading}>
+        <h1 className={styles.title}>
+          <span className={styles.preTitle}>A Louisville Based</span>
+          FullStack Developer
+        </h1>
+        <p>Creating intuitive and responsive software with people in mind</p>
+      </div>
+      <Image
+        src="/images/LUCAS-SPRING-3_websize.jpg"
+        alt="Image of me and my family"
+        layout="fill"
+      />
+    </div>
+  )
 }
 
-export  default Header
+export default Header
